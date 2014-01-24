1193a151b50d17e26dcc20004ab50c5d45c5db3c
luyonglie <luyonglie@163.com>
2014-01-24 23:54:22 +0800

    全部删除，重新上传

--- SuWenProject/SuWenProject/footer.php
+++
@@ -1,9 +0,0 @@
-<div>
-<div>
-<center><hr><h1>欢迎光临我的小站</h1><br>
-版权2014@copyright
-</center></div>
-
-</div>
-</body>
-</html>
--- SuWenProject/SuWenProject/head.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head><title>微通</title></head>
- 
-<body>
-<div>
-<center><h1>欢迎光临我的小站</h1><hr></center></div>
-
-
-
